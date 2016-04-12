f9b9f9cfd11a2fa648707b31161967ef5eeb2fff
Makapuf <makapuf2@gmail.com>
2016-04-13 00:05:48 +0200

    reindent code
    Linux-style, 4 spaces - using uncrustify

--- source/dcmo5boutons.h
+++ source/dcmo5boutons.h
@@ -1,16 +1,16 @@
-//////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////
 // DCMO5BOUTONS.H - definition des boutons
 // Author   : Daniel Coulom - danielcoulom@gmail.com
 // Web site : http://dcmo5.free.fr
 // Creation : Novembre 2007
 //
 // This file is part of DCMO5 v11.
-// 
+//
 // DCMO5 v11 is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // DCMO5 v11 is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -19,23 +19,23 @@
 // You should have received a copy of the GNU General Public License
 // along with DCMO5 v11.  If not, see <http://www.gnu.org/licenses/>.
 //
-//////////////////////////////////////////////////////////////////////////////
- 
+// ////////////////////////////////////////////////////////////////////////////
+
 button bouton[OTHERBUTTON_MAX] = {
-{"[haut]",   0, 0,  22, 22, 58}, //0x00 
-{"[gauche]", 0, 0,  22, 22, 59}, //0x01 
-{"[droite]", 0, 0,  22, 22, 60}, //0x02 
-{"[bas]",    0, 0,  22, 22, 61}, //0x03 
-{"Action",   0, 0,  44, 18, 62}, //0x04 
-{"[X]",      0, 0,  16, 15, 63}, //0x05
-{"[prev]",   0, 0,  16,  8, 64}, //0x06
-{"[next]",   0, 0,  16,  8, 65}, //0x07
-{"Options",  0, 0,  50, 16, 66}, //0x08
-{"?",        0, 0,  15, 16, 67}, //0x09
-{" ",        0, 0, 214, 22, 68}, //0x0a
-{"k7",       0, 0,  22, 16, 69}, //0x0b
-{"fd",       0, 0,  22, 16, 70}, //0x0c
-{"memo",     0, 0,  44, 16, 71}  //0x0d
+    {"[haut]",   0, 0,  22, 22, 58}, // 0x00
+    {"[gauche]", 0, 0,  22, 22, 59}, // 0x01
+    {"[droite]", 0, 0,  22, 22, 60}, // 0x02
+    {"[bas]",    0, 0,  22, 22, 61}, // 0x03
+    {"Action",   0, 0,  44, 18, 62}, // 0x04
+    {"[X]",      0, 0,  16, 15, 63}, // 0x05
+    {"[prev]",   0, 0,  16,  8, 64}, // 0x06
+    {"[next]",   0, 0,  16,  8, 65}, // 0x07
+    {"Options",  0, 0,  50, 16, 66}, // 0x08
+    {"?",        0, 0,  15, 16, 67}, // 0x09
+    {" ",        0, 0, 214, 22, 68}, // 0x0a
+    {"k7",       0, 0,  22, 16, 69}, // 0x0b
+    {"fd",       0, 0,  22, 16, 70}, // 0x0c
+    {"memo",     0, 0,  44, 16, 71}  // 0x0d
 };
 
 int flechegauche[16] = {0x00000000, 0x00000000, 0x00000000, 0x00000000,
